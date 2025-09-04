4d978648284daf7067c069df37d6ac7b1886ff53
CharlesOsang017 <charlesosango02@gmail.com>
2025-09-04 18:04:55 +0300

    Make hero section responsive

--- app/components/layout/Hero.tsx
+++ app/components/layout/Hero.tsx
@@ -7,35 +7,35 @@ const Hero = () => {
     <div className='bg-base-200 mt-30'>
       <div className='flex px-5 md:flex-row flex-col items-center gap-10'>
         <div className=''>
-          <h1 className='md:text-4xl font-bold'>
+          <h1 className='md:text-4xl font-bold md:text-start text-center'>
             Get more done with <span className='text-blue-700'>Chronos</span>
           </h1>
           <p className='py-3 text-muted-foreground'>
             The modern task management platform that helps teams organize, track
             and complete task efficiently.
           </p>
-          <div className='flex items-center gap-4 py-2'>
-            <Button className='bg-blue-700'>Try for Free</Button>
-            <Button variant={"outline"} className='border'>
+          <div className='flex items-center gap-4 py-2 justify-center md:justify-start'>
+            <Button className='bg-blue-700 text-xs'>Try for Free</Button>
+            <Button variant={"outline"} className='border text-xs'>
               See Features
             </Button>
           </div>
 
           <div className='flex items-center gap-2 py-4'>
             <div className='flex items-center gap-2'>
-              <CheckCircle size={15} />
+              <CheckCircle className="md:w-4 md:h-4 w-3 h-3" />
               <p className='text-xs text-muted-foreground'>
                 No credit card required
               </p>
             </div>
             <div className='flex items-center gap-2'>
-              <CheckCircle size={15} />
+              <CheckCircle className="md:w-4 md:h-4 w-3 h-3" />
               <p className='text-xs text-muted-foreground'>
                 Free plan available
               </p>
             </div>
             <div className='flex items-center gap-2'>
-              <CheckCircle size={15} />
+              <CheckCircle className="md:w-4 md:h-4 w-3 h-3" />
               <p className='text-xs text-muted-foreground'>Cancel anytime</p>
             </div>
           </div>
